3b633f2d01d347300e138533d2ba658ec16912c6
R3DG0BL1N <58450310+R3DG0BL1N@users.noreply.github.com>
2025-11-19 19:44:32 +0100

    First duck on deck

---
+++ bin/watch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+projects=("main")
+
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
+
+for proj in "${projects[@]}"; do
+    sass --watch "$proj/src/resources":"../$proj/public/resources" --style=compressed &
+    chokidar "$proj/src/resources/*.js" -c "javascript-obfuscator ../$proj/src/resources --output ../$proj/public/resources --compact true --self-defending true > /dev/null 2>&1" &
+done
+
+wait
