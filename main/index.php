6498e232279c2ab8e32356432ba2f96dcc0763bb
R3DG0BL1N <58450310+R3DG0BL1N@users.noreply.github.com>
2025-11-24 18:33:06 +0100

    refactor(): Modify skeleton for new core classes

--- main/index.php
+++ main/index.php
@@ -3,12 +3,13 @@
 declare(strict_types=1);
 require __DIR__ . "/../vendor/autoload.php";
 
-use SmartGoblin\Slaves\KernelSlave;
+use SmartGoblin\Components\Core\Server;
 use SmartGoblin\Components\Core\Config;
 use SmartGoblin\Components\Router\Endpoint;
 
 $config = Config::new(__DIR__, "main", false);
 $config->configureAllowedHosts(["127.0.0.1", "localhost"]);
+$config->configureAuthorization("SMRTGOBLESSION", 7, "localhost");
 $config->configureUnauthorizedRedirects("/login", "main");
 
 $config->configureApi([
@@ -18,8 +19,8 @@ $config->configureView([
     Endpoint::view(false, "/", "main")
 ]);
 
-$slave = KernelSlave::call();
-$slave->order($config);
-$slave->work();
+$server = Server::new();
+$server->configure($config);
+$server->run();
 
 ?>
--- main/src/api/Health.php
+++ main/src/api/Health.php
@@ -3,6 +3,9 @@
 use SmartGoblin\Components\Http\Request;
 use SmartGoblin\Components\Http\Response;
 
+use SmartGoblin\Worker\LogWorker;
+
 return function(Request $request): Response {
+    LogWorker::log("Health API call was processed successfully");
     return Response::new(true, 200);
 };
