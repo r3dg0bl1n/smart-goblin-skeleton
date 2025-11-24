aff1a263cc987723b380c46f65ae294f43c9ec53
R3DG0BL1N <58450310+R3DG0BL1N@users.noreply.github.com>
2025-11-24 12:24:11 +0100

    fix(watch.sh): Fixed project folder for resources

--- bin/watch.sh
+++ bin/watch.sh
@@ -5,8 +5,8 @@ projects=("main")
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 for proj in "${projects[@]}"; do
-    sass --watch "$proj/src/resources":"../$proj/public/resources" --style=compressed &
-    chokidar "$proj/src/resources/*.js" -c "javascript-obfuscator ../$proj/src/resources --output ../$proj/public/resources --compact true --self-defending true > /dev/null 2>&1" &
+    sass --watch "$proj/src/resources":"$proj/public/resources" --style=compressed &
+    chokidar "$proj/src/resources/*.js" -c "javascript-obfuscator $proj/src/resources --output $proj/public/resources --compact true --self-defending true > /dev/null 2>&1" &
 done
 
 wait
